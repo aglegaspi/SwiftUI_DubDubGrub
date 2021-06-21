aab13ee8d095c464423386c398e0e0dc6182120e
aglegaspi <lex83nyc@gmail.com>
2021-06-21 08:15:42 -0400

    create app

---
+++ SwiftUI_DubDubGrub/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 6/20/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_DubDubGrub/SwiftUI_DubDubGrubApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DubDubGrubApp.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 6/20/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DubDubGrubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
