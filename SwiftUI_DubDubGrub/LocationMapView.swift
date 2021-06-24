f841cbf83c78fa5d654459076080f6cd820077d4
aglegaspi <lex83nyc@gmail.com>
2021-06-24 09:03:25 -0400

    set up MapView

--- SwiftUI_DubDubGrub/LocationMapView.swift
+++ SwiftUI_DubDubGrub/LocationMapView.swift
@@ -6,10 +6,31 @@
 //
 
 import SwiftUI
+import MapKit
 
 struct LocationMapView: View {
+    
+    @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                                  longitude: -121.891054),
+                                                   //How much to zoom in
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
     var body: some View {
-        Text("Location MapView")
+        
+        ZStack {
+            Map(coordinateRegion: $region)
+                .ignoresSafeArea()
+            
+            VStack {
+                Image("ddg-map-logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 70)
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+                Spacer()
+            }
+        }
+        
+        
     }
 }
 
