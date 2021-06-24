ee3e304d612f56b85c3f3eef07a1d126b5588533
aglegaspi <lex83nyc@gmail.com>
2021-06-24 08:10:48 -0400

    add brandPrimary color

---
+++ SwiftUI_DubDubGrub/Color+Ext.swift
@@ -0,0 +1,12 @@
+//
+//  Color+Ext.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 6/24/21.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
