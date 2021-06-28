7d1b2e9c32bbc3d18a890462a00f5aa811a589cb
aglegaspi <lex83nyc@gmail.com>
2021-06-28 10:20:16 -0400

    add List to LocationListView and subview

--- SwiftUI_DubDubGrub/LocationListView.swift
+++ SwiftUI_DubDubGrub/LocationListView.swift
@@ -9,12 +9,52 @@ import SwiftUI
 
 struct LocationListView: View {
     var body: some View {
-        Text("List View")
+        NavigationView {
+            List {
+                ForEach(0..<10) { item in
+                    HStack {
+                        Image("default-square-asset")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 80, height: 80, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+                            .clipShape(Circle())
+                            .padding(.vertical, 8)
+                        
+                        VStack(alignment: .leading) {
+                            Text("Hello")
+                                .font(.title2)
+                                .fontWeight(.semibold)
+                                .lineLimit(1)
+                                .minimumScaleFactor(0.75)
+                            
+                            HStack {
+                                ForEach(0..<5) { item in
+                                    AvatarView()
+                                }
+                            }
+                        } //VStack
+                        .padding(.leading)
+                    }
+                }
+            }
+            .navigationTitle("Grub Spots")
+        }
+        
     }
 }
 
 struct LocationListView_Previews: PreviewProvider {
     static var previews: some View {
         LocationListView()
+    }
+}
+
+struct AvatarView: View {
+    var body: some View {
+        Image("default-avatar")
+            .resizable()
+            .scaledToFit()
+            .frame(width: 35, height: 35, alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/)
+            .clipShape(Circle())
     }
 }
