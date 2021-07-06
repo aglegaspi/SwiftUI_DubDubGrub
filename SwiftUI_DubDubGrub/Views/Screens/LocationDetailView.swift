b5578f50124b7225a57e6cdab3e1a61f1f9e0743
aglegaspi <lex83nyc@gmail.com>
2021-07-06 08:09:40 -0400

    file organization

--- SwiftUI_DubDubGrub/Views/Screens/LocationDetailView.swift
+++ SwiftUI_DubDubGrub/Views/Screens/LocationDetailView.swift
@@ -13,24 +13,18 @@ struct LocationDetailView: View {
     
     var body: some View {
             VStack(spacing: 16) {
-                Image("default-banner-asset")
-                    .resizable()
-                    .scaledToFill()
-                    .frame(height: 120)
+                BannerImageView(imageName: "default-banner-asset")
                 
                 HStack {
                     Label("123 Main Street", systemImage: "mappin.and.ellipse")
                         .font(.caption)
                         .foregroundColor(.secondary)
+                    
                     Spacer()
                 }
                 .padding(.horizontal)
                 
-                Text("This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. ")
-                    .lineLimit(3)
-                    .minimumScaleFactor(0.75)
-                    .frame(height: 70)
-                    .padding(.horizontal)
+                DescriptionView(text: "This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. This is a test description. ")
                 
                 ZStack {
                     Capsule()
@@ -127,5 +121,30 @@ struct FirstNameAvatarView: View {
                 .lineLimit(1)
                 .minimumScaleFactor(0.75)
         }
+    }
+}
+
+struct BannerImageView: View {
+    
+    var imageName: String
+    
+    var body: some View {
+        Image(imageName)
+            .resizable()
+            .scaledToFill()
+            .frame(height: 120)
+    }
+}
+
+struct DescriptionView: View {
+    
+    var text: String
+    
+    var body: some View {
+        Text(text)
+            .lineLimit(3)
+            .minimumScaleFactor(0.75)
+            .frame(height: 70)
+            .padding(.horizontal)
     }
 }
