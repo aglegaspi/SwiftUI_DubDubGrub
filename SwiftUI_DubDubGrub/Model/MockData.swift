d1370a3343ef6ada4f66bc064ee8d9b526d460d7
aglegaspi <lex83nyc@gmail.com>
2021-07-08 18:37:19 -0400

    creat Mock Data model

---
+++ SwiftUI_DubDubGrub/Model/MockData.swift
@@ -0,0 +1,23 @@
+//
+//  MockData.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 7/8/21.
+//
+
+import CloudKit
+
+struct MockData {
+    
+    static var location: CKRecord {
+        let record = CKRecord(recordType: "DDGLocation")
+        record[DDGLocation.kName]           = "Alxndr's Bar and Grill"
+        record[DDGLocation.kAddress]        = "123 Developer Way"
+        record[DDGLocation.kDescription]    = "You are a Porfessional iOS Developer"
+        record[DDGLocation.kWebsiteURL]     = "http://www.dribbble.com"
+        record[DDGLocation.kLocation]       = CLLocation(latitude: 37.33156, longitude: -121.891054)
+        record[DDGLocation.kPhoneNumber]    = "347-903-1847"
+        
+        return record
+    }
+}
