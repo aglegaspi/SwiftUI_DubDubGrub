886673c6a6c0f1a4cd6b8548f29cc2eddc023250
aglegaspi <lex83nyc@gmail.com>
2021-07-13 18:43:05 -0400

    create Alert Context:

--- SwiftUI_DubDubGrub/Utilities/AlertItem.swift
+++ SwiftUI_DubDubGrub/Utilities/AlertItem.swift
@@ -14,3 +14,10 @@ struct AlertItem: Identifiable {
     let dissmissButton: Alert.Button
 }
 
+struct AlertContext {
+    
+    //MARK: - MapView Errors
+    static let unableToGetLocations = AlertItem(title: Text("Locations Error"),
+                                                message: Text("Unable to retrieve locations."),
+                                                dissmissButton: .default(Text("OK")))
+}
