a8c3349f6a1178a62015a35f9e83cabd45c0051b
aglegaspi <lex83nyc@gmail.com>
2021-09-02 08:17:15 -0400

    add PlaceholderImage enum

--- SwiftUI_DubDubGrub/Utilities/Constants.swift
+++ SwiftUI_DubDubGrub/Utilities/Constants.swift
@@ -5,9 +5,15 @@
 //  Created by Alex 6.1 on 7/12/21.
 //
 
-import Foundation
+import UIKit
 
 enum RecordType {
     static let location = "DDGLocation"
     static let profile = "DDGProfile"
+}
+
+enum PlaceholderImage {
+    static let avatar = UIImage(named: "default-avatar")
+    static let square = UIImage(named: "default-square-asset")
+    static let banner = UIImage(named: "default-banner-asset")
 }
