226bcc3c6ea7903c51448dd7470d69f8ea400649
aglegaspi <lex83nyc@gmail.com>
2021-09-02 08:56:02 -0400

    update banner image sources

--- SwiftUI_DubDubGrub/Views/Screens/LocationDetailView.swift
+++ SwiftUI_DubDubGrub/Views/Screens/LocationDetailView.swift
@@ -15,7 +15,7 @@ struct LocationDetailView: View {
     
     var body: some View {
             VStack(spacing: 16) {
-                BannerImageView(imageName: "default-banner-asset")
+                BannerImageView(image: location.createBannerImage())
                 
                 HStack {
                     Label(location.address, systemImage: "mappin.and.ellipse")
@@ -128,10 +128,10 @@ struct FirstNameAvatarView: View {
 
 struct BannerImageView: View {
     
-    var imageName: String
+    var image: UIImage
     
     var body: some View {
-        Image(imageName)
+        Image(uiImage: image)
             .resizable()
             .scaledToFill()
             .frame(height: 120)
