36a69ddfd588ad61d2457420e44687c2b41ed61b
aglegaspi <lex83nyc@gmail.com>
2021-09-02 08:37:11 -0400

    extension to convert CKAsset to UIImage

--- SwiftUI_DubDubGrub/Extensions/CKAsset+Ext.swift
+++ SwiftUI_DubDubGrub/Extensions/CKAsset+Ext.swift
@@ -5,4 +5,20 @@
 //  Created by Alex 6.1 on 9/2/21.
 //
 
-import Foundation
+import CloudKit
+import UIKit
+
+extension CKAsset {
+    func convertToUIImage(in dimension: ImageDimension) -> UIImage {
+        let placeholder = ImageDimension.getPlaceholder(for: dimension)
+        
+        guard let imageURL = self.fileURL else { return placeholder }
+        
+        do {
+            let data = try Data(contentsOf: imageURL)
+            return UIImage(data: data) ?? placeholder
+        } catch {
+            return placeholder
+        }
+    }
+}
