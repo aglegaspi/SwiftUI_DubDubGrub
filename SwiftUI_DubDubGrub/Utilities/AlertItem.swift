2ac7cb751a6c1d83f04fa54e3cb198357d25130f
aglegaspi <lex83nyc@gmail.com>
2021-09-03 20:25:21 -0400

    create alerts for Location Manager

--- SwiftUI_DubDubGrub/Utilities/AlertItem.swift
+++ SwiftUI_DubDubGrub/Utilities/AlertItem.swift
@@ -20,4 +20,16 @@ struct AlertContext {
     static let unableToGetLocations = AlertItem(title: Text("Locations Error"),
                                                 message: Text("Unable to retrieve locations."),
                                                 dissmissButton: .default(Text("OK")))
+    
+    static let locationRestricted = AlertItem(title: Text("Locations Restricted"),
+                                                message: Text("Location restricted. Possible to parental controls."),
+                                                dissmissButton: .default(Text("OK")))
+    
+    static let locationDenied = AlertItem(title: Text("Locations Denied"),
+                                                message: Text("This app does not have access to your location. To change that go to Settings > Dub Dub Grub > Location"),
+                                                dissmissButton: .default(Text("OK")))
+    
+    static let locationDisabled = AlertItem(title: Text("Location Services Disabled"),
+                                                message: Text("This app location services are disabled. To change that go to Settings > Privacy > Location Services"),
+                                                dissmissButton: .default(Text("OK")))
 }
