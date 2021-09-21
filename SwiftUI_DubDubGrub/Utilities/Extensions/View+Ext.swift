95e9186f6215712c1b1f1aac3d5befb852bcc237
aglegaspi <lex83nyc@gmail.com>
2021-09-21 12:48:42 -0400

    create dismiss keyboard extension

---
+++ SwiftUI_DubDubGrub/Utilities/Extensions/View+Ext.swift
@@ -0,0 +1,20 @@
+//
+//  View+Ext.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 7/6/21.
+//
+
+import SwiftUI
+
+extension View {
+    
+    func profileNameStyle() -> some View {
+        self.modifier(ProfileNameText())
+    }
+    
+    // dismissed the keyboard when this function is called
+    func dismissKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
