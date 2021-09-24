8ca4db66aa8a89357dab0a6912ed3d26fd3167c6
aglegaspi <lex83nyc@gmail.com>
2021-09-24 09:47:55 -0700

    get url path and compress jpeg

--- SwiftUI_DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
+++ SwiftUI_DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
@@ -12,10 +12,16 @@ extension UIImage {
     func convertToCKAsset() -> CKAsset? {
         
         // get apps base document directory url
+        guard let urlPath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            print("Document Directory URL came back nil")
+            return nil
+        }
         
         // append unique identifier for our profile image
+        let fileURL = urlPath.appendingPathComponent("selectedAvatarImage")
         
         // write the image data to the location the address points to
+        let imageData = jpegData(compressionQuality: 0.25)
         
         // create our CKAsset with fileURL
         
