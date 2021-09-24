fe5215db2e836be303215864572af597fbc3be08
aglegaspi <lex83nyc@gmail.com>
2021-09-24 09:42:05 -0700

    create UIImage extension to convert data to CKAsset

---
+++ SwiftUI_DubDubGrub/Utilities/Extensions/UIImage+Ext.swift
@@ -0,0 +1,27 @@
+//
+//  UIImage+Ext.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 9/23/21.
+//
+
+import CloudKit
+import UIKit
+
+extension UIImage {
+    func convertToCKAsset() -> CKAsset? {
+        
+        // get apps base document directory url
+        
+        // append unique identifier for our profile image
+        
+        // write the image data to the location the address points to
+        
+        // create our CKAsset with fileURL
+        
+        
+        
+        return nil
+    }
+    
+}
