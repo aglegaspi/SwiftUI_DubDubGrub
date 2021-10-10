8401a9564fc6e87d727041317d7e7ac653edc567
aglegaspi <lex83nyc@gmail.com>
2021-10-10 19:46:18 -0400

    set up progress view

--- SwiftUI_DubDubGrub/Views/CustomViews/LoadingView.swift
+++ SwiftUI_DubDubGrub/Views/CustomViews/LoadingView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct LoadingView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color(.systemBackground)
+                .opacity(0.9)
+                .ignoresSafeArea()
+            ProgressView()
+                .progressViewStyle(CircularProgressViewStyle(tint: .brandPrimary))
+                .scaleEffect(5)
+                .offset(y: -40)
+        }
     }
 }
 
