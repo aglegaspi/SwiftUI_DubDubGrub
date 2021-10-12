81eded261be71b0da932432cd98ae7c590504242
aglegaspi <lex83nyc@gmail.com>
2021-10-12 17:01:13 -0400

    create new group and LocationDetailViewModel

---
+++ SwiftUI_DubDubGrub/Views/Screens/LocationDetailView/LocationDetailViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LocationDetailViewModel.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 10/12/21.
+//
+
+import Foundation
+
+final class LocationDetailViewModel: ObservableObject {
+    
+    
+}
