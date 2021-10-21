8eafc8402764b502fae0226c5ff47bceb668e487
aglegaspi <lex83nyc@gmail.com>
2021-10-21 10:20:59 -0400

    create ProfileModalView

---
+++ SwiftUI_DubDubGrub/Views/Screens/LocationDetailView/ProfileModalView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileModalView.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 10/21/21.
+//
+
+import SwiftUI
+
+struct ProfileModalView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProfileModalView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProfileModalView()
+    }
+}
