9714439bc65c21232c31df590a925348081bbbcf
aglegaspi <lex83nyc@gmail.com>
2021-10-24 12:33:11 -0700

    use ternary stmt to set checkin status

--- SwiftUI_DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
+++ SwiftUI_DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
@@ -50,7 +50,7 @@ struct LocationDetailView: View {
                         }
                         
                         Button {
-                            viewModel.updateCheckInStatus(to: .checkedOut)
+                            viewModel.updateCheckInStatus(to: viewModel.isCheckedIn ? .checkedOut : .checkedIn)
                         } label: {
                             LocationActionButton(color: .brandPrimary, imageName: "person.fill.checkmark")
                         }
