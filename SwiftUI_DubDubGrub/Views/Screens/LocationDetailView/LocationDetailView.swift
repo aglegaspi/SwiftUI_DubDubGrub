e92044d77d0379e500fa6833ef2e28f7b3db5960
aglegaspi <lex83nyc@gmail.com>
2021-10-24 12:47:30 -0700

    add ZStack

--- SwiftUI_DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
+++ SwiftUI_DubDubGrub/Views/Screens/LocationDetailView/LocationDetailView.swift
@@ -62,26 +62,29 @@ struct LocationDetailView: View {
                     .bold()
                     .font(.title2)
                 
-                if viewModel.checkedInProfiles.isEmpty {
-                    Text("Nobody's Here 🥸")
-                        .bold()
-                        .font(.title3)
-                        .foregroundColor(.secondary)
-                        .padding(.top, 30)
-                } else {
-                    ScrollView {
-                        LazyVGrid(columns: viewModel.columns, content: {
-                            
-                            ForEach(viewModel.checkedInProfiles) { profile in
-                                FirstNameAvatarView(profile: profile)
-                                    .onTapGesture {
-                                        viewModel.isShowingProfileModal = true
-                                    }
-                            }
-                            
-                        })
-                    } //scrollview
-                } //else
+                ZStack {
+
+                    if viewModel.checkedInProfiles.isEmpty {
+                        Text("Nobody's Here 🥸")
+                            .bold()
+                            .font(.title3)
+                            .foregroundColor(.secondary)
+                            .padding(.top, 30)
+                    } else {
+                        ScrollView {
+                            LazyVGrid(columns: viewModel.columns, content: {
+                                
+                                ForEach(viewModel.checkedInProfiles) { profile in
+                                    FirstNameAvatarView(profile: profile)
+                                        .onTapGesture {
+                                            viewModel.isShowingProfileModal = true
+                                        }
+                                }
+                                
+                            })
+                        } //scrollview
+                    } //else
+                }
                 
 
                 Spacer()
