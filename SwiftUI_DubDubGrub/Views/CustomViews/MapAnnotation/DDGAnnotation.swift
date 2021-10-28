01027220e71f68da9b4a58cde5b46e3404e5904c
aglegaspi <lex83nyc@gmail.com>
2021-10-27 17:56:20 -0700

    create DDGAnnotation view

---
+++ SwiftUI_DubDubGrub/Views/CustomViews/MapAnnotation/DDGAnnotation.swift
@@ -0,0 +1,20 @@
+//
+//  DDGAnnotation.swift
+//  SwiftUI_DubDubGrub
+//
+//  Created by Alex 6.1 on 10/27/21.
+//
+
+import SwiftUI
+
+struct DDGAnnotation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DDGAnnotation_Previews: PreviewProvider {
+    static var previews: some View {
+        DDGAnnotation()
+    }
+}
