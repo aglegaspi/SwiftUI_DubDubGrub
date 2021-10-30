fcb70a5584daa9caeb9f60c60b9d5ca027ce6cbb
aglegaspi <lex83nyc@gmail.com>
2021-10-30 09:13:39 -0700

    add accessibility init to not read filename string

--- SwiftUI_DubDubGrub/Views/CustomViews/LogoView.swift
+++ SwiftUI_DubDubGrub/Views/CustomViews/LogoView.swift
@@ -12,7 +12,8 @@ struct LogoView: View {
     var frameWidth: CGFloat
     
     var body: some View {
-        Image("ddg-map-logo")
+        // initializes image so the string is not read during voice over (accessibility)
+        Image(decorative: "ddg-map-logo")
             .resizable()
             .scaledToFit()
             .frame(width: frameWidth)
