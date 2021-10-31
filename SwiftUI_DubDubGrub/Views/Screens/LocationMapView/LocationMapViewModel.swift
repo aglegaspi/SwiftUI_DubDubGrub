24842312c2ab13e155930146db78cb4ea0132792
aglegaspi <lex83nyc@gmail.com>
2021-10-31 09:31:15 -0700

    encapsulate LMVM for sole access to LMV

--- SwiftUI_DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
+++ SwiftUI_DubDubGrub/Views/Screens/LocationMapView/LocationMapViewModel.swift
@@ -9,49 +9,54 @@ import SwiftUI
 import CloudKit
 import MapKit
 
-final class LocationMapViewModel: ObservableObject {
+extension LocationMapView {
     
-    @Published var checkedInProfiles: [CKRecord.ID: Int] = [:]
-    @Published var isShowingDetailView = false
-    @Published var alertItem: AlertItem?
-    @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
-                                                                              longitude: -121.891054),
-                                               //How much to zoom in
-                                               span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
-    
-    func getLocations(for locationManager: LocationManager) {
-        CloudKitManager.shared.getLocations { result in
-            // update UI on the main thread. Each update triggers an update on the UI
-            DispatchQueue.main.async {
-                switch result {
-            case .success(let locations):
-                locationManager.locations = locations
-            case .failure(_):
-                self.alertItem = AlertContext.unableToGetLocations
-            }}
-        }
-    }
-    
-    func getCheckedInCount() {
-        CloudKitManager.shared.getCheckedInProfilesCount { result in
-            DispatchQueue.main.async {
-                switch result {
-                case .success(let checkedInProfiles):
-                    self.checkedInProfiles = checkedInProfiles
+    final class LocationMapViewModel: ObservableObject {
+        
+        @Published var checkedInProfiles: [CKRecord.ID: Int] = [:]
+        @Published var isShowingDetailView = false
+        @Published var alertItem: AlertItem?
+        @Published var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 37.331516,
+                                                                                  longitude: -121.891054),
+                                                   //How much to zoom in
+                                                   span: MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01))
+        
+        func getLocations(for locationManager: LocationManager) {
+            CloudKitManager.shared.getLocations { result in
+                // update UI on the main thread. Each update triggers an update on the UI
+                DispatchQueue.main.async {
+                    switch result {
+                case .success(let locations):
+                    locationManager.locations = locations
                 case .failure(_):
-                    print("no count available")
+                    self.alertItem = AlertContext.unableToGetLocations
+                }}
+            }
+        }
+        
+        
+        func getCheckedInCount() {
+            CloudKitManager.shared.getCheckedInProfilesCount { result in
+                DispatchQueue.main.async {
+                    switch result {
+                    case .success(let checkedInProfiles):
+                        self.checkedInProfiles = checkedInProfiles
+                    case .failure(_):
+                        print("no count available")
+                    }
                 }
             }
         }
-    }
-    
-    @ViewBuilder func createLocationDetailView(for location: DDGLocation, in sizeCategory: ContentSizeCategory) -> some View {
-        if sizeCategory >= .accessibilityLarge {
-            LocationDetailView(viewModel: LocationDetailViewModel(location: location)).embedInScrollView()
-        } else {
-            LocationDetailView(viewModel: LocationDetailViewModel(location: location))
+        
+        
+        @ViewBuilder func createLocationDetailView(for location: DDGLocation, in sizeCategory: ContentSizeCategory) -> some View {
+            if sizeCategory >= .accessibilityLarge {
+                LocationDetailView(viewModel: LocationDetailViewModel(location: location)).embedInScrollView()
+            } else {
+                LocationDetailView(viewModel: LocationDetailViewModel(location: location))
+            }
         }
+        
     }
     
 }
-
