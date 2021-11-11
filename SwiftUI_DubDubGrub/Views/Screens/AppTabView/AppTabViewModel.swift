69e20ee5d1d1d66c2774057fd98a50b72491f042
aglegaspi <lex83nyc@gmail.com>
2021-11-11 08:52:12 -0800

    delete location checks from this model

--- SwiftUI_DubDubGrub/Views/Screens/AppTabView/AppTabViewModel.swift
+++ SwiftUI_DubDubGrub/Views/Screens/AppTabView/AppTabViewModel.swift
@@ -10,70 +10,21 @@ import CoreLocation
 
 extension AppTabView {
     
-    final class AppTabViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    final class AppTabViewModel: ObservableObject {
         
         @Published var isShowingOnboardView = false
-        @Published var alertItem: AlertItem?
         @AppStorage("hasSeenOnboardView") var hasSeenOnboardView = false {
             didSet { isShowingOnboardView = hasSeenOnboardView }
         }
-        
-        // create this instance if Location Services are available
-        var deviceLocationManager: CLLocationManager?
+    
         // constant for userdefault key
         var kHasSeenOnboardView = "hasSeenOnboardView"
 
-        
         //checks if the user's seen onboarding screen. false - load view. true - check location services.
-        func runStartupChecks() {
-            if !hasSeenOnboardView {
-                hasSeenOnboardView = true
-            } else {
-                checkIfLocationServicesIsEnabled()
-            }
+        func checkIfHasSeenOnboard() {
+            if !hasSeenOnboardView {  hasSeenOnboardView = true }
         }
-        
-        
-        // check if Location Services are enable and create the instance
-        func checkIfLocationServicesIsEnabled() {
-            if CLLocationManager.locationServicesEnabled() {
-                deviceLocationManager = CLLocationManager()
-                
-                // force unwrapped because the instance is created at this point
-                deviceLocationManager!.delegate = self
-            } else {
-                alertItem = AlertContext.locationDisabled
-            }
-        }
-        
-        
-        // check for app specific permission
-        private func checkLocationAuthorization() {
-            guard let deviceLocationManager = deviceLocationManager else { return }
-            
-            switch deviceLocationManager.authorizationStatus {
-                
-            case .notDetermined:
-                deviceLocationManager.requestWhenInUseAuthorization()
-            case .restricted:
-                alertItem = AlertContext.locationRestricted
-                return
-            case .denied:
-                alertItem = AlertContext.locationDenied
-                return
-            case .authorizedAlways, .authorizedWhenInUse:
-                break
-            @unknown default:
-                break
-            }
-        }
-        
-        
-        func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
-            checkLocationAuthorization()
-        }
-        
-        
+     
     } //apptabviewmodel
     
 } //extension
